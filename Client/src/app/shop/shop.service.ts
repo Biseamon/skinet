14e118d642a50a755db097beced9fae3c13a2813
mbc <marin.capranov@bbconsult.co.uk>
2021-05-20 00:16:57 +0100

    adding routing and lazy loading

--- Client/src/app/shop/shop.service.ts
+++ Client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/productType';
 import { map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,10 @@ export class ShopService {
   baseUrl = "https://localhost:5001/api/";
 
   constructor(private http: HttpClient) { }
+
+  getProd(id: number){
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
+  }
 
   getProduct(shopParams: ShopParams) {
 
