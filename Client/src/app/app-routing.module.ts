a54f517ce7b5f1b5b4ac69b9346cff99d5004e2b
mbc <marin.capranov@bbconsult.co.uk>
2021-05-20 22:48:20 +0100

    adding loading spinner, different theme using bootswatch, a bit of styling...

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -6,12 +6,12 @@ import { TestEsrrorComponent } from './core/test-esrror/test-esrror.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: "", component: HomeComponent},
-  {path: "test-error", component: TestEsrrorComponent},
-  {path: "server-error", component: ServerErrorComponent},
-  {path: "not-found", component: NotFoundComponent},
-  {path: "shop", loadChildren: () => import("./shop/shop.module").then(mod => mod.ShopModule)},
-  {path: "**", redirectTo: "", pathMatch: "full"}
+  {path: "", component: HomeComponent, data: {breadcrumb: "Home"}},
+  {path: "test-error", component: TestEsrrorComponent, data: {breadcrumb: "Test Errors"}},
+  {path: "server-error", component: ServerErrorComponent, data: {breadcrumb: "Server Errors"}},
+  {path: "not-found", component: NotFoundComponent, data: {breadcrumb: "Not Found"}},
+  {path: "shop", loadChildren: () => import("./shop/shop.module").then(mod => mod.ShopModule), data: {breadcrumb: "Shop"}},
+  {path: "**", redirectTo: "not-found", pathMatch: "full"}
 ];
 
 @NgModule({
